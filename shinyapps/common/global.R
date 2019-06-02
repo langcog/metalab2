fd25490bbbcd81dd6163df25079cf0df82accf3a
cissard <cecile.issard@etu.parisdescartes.fr>
2019-06-02 19:53:21 +0200

    remove personal path

--- shinyapps/common/global.R
+++ shinyapps/common/global.R
@@ -11,8 +11,7 @@ library(plotly)
 
 project_directory <- "/srv/shiny-server/common/" # path using new deploy script that copies data to the shiny server global directory
 #project_directory <- "/home/metalab/metalab2/" # path using old deploy script
-#project_directory <- "/Users/Cecile/Documents/Metalab/metalab2/" # path for development purposes
-# project_directory <- "../../" # path for development purposes
+project_directory <- "../../" # path for development purposes
 
 
 
