5dcafead830394d33ea1a92bea8b2d678adf47bf
Erik Iverson <erik@sigmafield.org>
2020-10-07 08:46:45 -0700

    create content/dataset in build script

--- build/build-metalab-site.R
+++ build/build-metalab-site.R
@@ -7,6 +7,7 @@ load_cached_data(here("shinyapps", "site_data", "Rdata", "metalab.Rdata"))
 
 ## build dataset Rmd files from Rmd template, filling in each value of dataset.
 dataset_template <- readLines(here("build", "dataset-template.Rmd"))
+dir.create(here("content", "dataset"))
 lapply(dataset_info$short_name, function(s_name) {
   current_dataset <- dataset_info %>% filter(short_name == s_name)
   to_write <- sapply(dataset_template, function(template_line) {
@@ -20,6 +21,7 @@ lapply(dataset_info$short_name, function(s_name) {
                           template_line)
     gsub("<<DOMAIN_NAME>>", current_dataset$domain, template_line)
   })
+
   dir.create(here("content", "dataset", s_name))
   cat(to_write,
       file = here("content", "dataset", s_name, "index.Rmd"),
